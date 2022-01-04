43bc5ad1a0fdfbf911b2b4c2d48a19bf4cba8753
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-01-04 22:12:32 +0900

    person sort

--- second/person.ml
+++ second/person.ml
@@ -47,3 +47,21 @@ let test = count_ketsueki_A [person2] = 0
 let test = count_ketsueki_A [person1; person2] = 1
 let test = count_ketsueki_A [person1; person2; person3] = 2
 
+let rec person_insert lst person = match lst with
+  [] -> [person]
+  | {name = name_in_list; height = height; weight = weight; birth_day = birth_day; blood_type = blood_type} as first :: rest -> (match person with 
+                                        {name = name_in_argument}
+                                        -> if name_in_argument <= name_in_list then person :: lst
+                                                                               else first :: (person_insert rest person))
+
+let test = person_insert [] person1 = [person1]
+let test = person_insert [person3; person2] person1 = [person3; person2; person1]
+let test = person_insert [person3; person1] person2 = [person3; person2; person1]
+
+let rec person_sort lst = match lst with
+  [] -> []
+  | first :: rest -> person_insert (person_sort rest) first
+
+let test = person_sort [] = []
+let test = person_sort [person1] = [person1]
+let test = person_sort [person1; person2; person3] = [person3; person2; person1]
